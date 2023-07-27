d308b8adecd01ed0dfc1050ae6fa8db6eae0a831
NameNOTHING <129465555+NameNOTHING@users.noreply.github.com>
2023-07-28 02:03:16 +1000

    Implement Download Blockchain and Admin only button
    Added a button that links to the json file of the blockchain, and a Deletion Request button that only appears if the user is an Admin

--- secboardfront/src/components/HomePage.vue
+++ secboardfront/src/components/HomePage.vue
@@ -36,6 +36,28 @@
 					</div>
 				</div>
 			</div>
+			<a href="https://github.com/Dragon44588/CSIT321-SecBoard/blob/main/database.JSON" target="_blank" style="text-decoration: none; color:white; background-color:white">
+			<div @click="goDownloadBlockchain" class="aside-download-blockchain" style="height: 40px; margin-top: 20px; display: flex; justify-content: center; align-items: center; cursor: pointer">
+				<div style="display: flex; align-items: center; width: 100%">
+					<div style="width: 30px; margin-left: 20px; display: flex; justify-content: center; align-items: center">
+						<img style="height: 20x; width: 20px" src="../../public/download-outline.svg" />
+					</div>
+					<div style="flex: 1; margin-left: 5px">
+						<strong>Download Blockchain</strong>
+					</div>
+				</div>
+			</div>
+			</a>
+			<div v-if="myname=='Admin'" @click="goDeletionRequests" class="aside-deletion-requests" style="height: 40px; margin-top: 20px; display: flex; justify-content: center; align-items: center; cursor: pointer">
+				<div style="display: flex; align-items: center; width: 100%">
+					<div style="width: 30px; margin-left: 20px; display: flex; justify-content: center; align-items: center">
+						<img style="height: 20x; width: 20px" src="../../public/deletion-request.svg" />
+					</div>
+					<div style="flex: 1; margin-left: 5px">
+						<strong>Deletion Requests</strong>
+					</div>
+				</div>
+			</div>
 			<div @click="goLogOut" class="aside-log-out" style="height: 40px; margin-top: 20px; display: flex; justify-content: center; align-items: center; cursor: pointer">
 				<div style="display: flex; align-items: center; width: 100%">
 					<div style="width: 30px; margin-left: 20px; display: flex; justify-content: center; align-items: center">
@@ -69,6 +91,9 @@ function goMyPosts() {
 }
 function goAddNewPost() {
 	router.push({ path: "/home/add-new-post" });
+}
+function goDeletionRequests() {
+	router.push({ path: "/home/deletion-requests" });
 }
 function goLogOut() {
 	sessionStorage.clear();
@@ -106,6 +131,12 @@ function goLogOut() {
 	background-color: #082c39;
 }
 .aside-log-out:hover {
+	background-color: #082c39;
+}
+.aside-download-blockchain:hover {
+	background-color: #082c39;
+}
+.aside-deletion-requests:hover {
 	background-color: #082c39;
 }
 </style>
